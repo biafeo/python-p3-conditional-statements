995cefab50ceb455460053fdf8c1091e0676d310
beatriz <beatrizffeo@gmail.com>
2024-04-19 21:51:39 -0600

    lab complete;
    git commit -m labcomplete

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,53 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username == 'ADMIN' and password == "12345": 
+        message = "Access granted"
+    elif username == 'admin' and password == '12345':
+        message = "Access granted"
+    else:
+        message = "Access denied"
+    return message
+
+
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        message = "It's brisk out there!"
+    elif temperature >= 40 and temperature <= 65 :
+        message = "It's a little chilly out there!"
+    elif temperature > 85 : 
+        message = "It's too dang hot out there!"
+    else :
+        message =   "It's perfect out there!"
+    return message
+
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 ==0:
+        message = "FizzBuzz"
+    elif num % 3 == 0:
+        message = "Fizz"
+    elif num %5 == 0:
+        message = "Buzz"
+    else :
+        message = num
+    return message
+
+
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == '+' :
+        message = num1 +num2
+    elif operation == "-":
+        message = num1 - num2
+    elif operation == "*":
+        message = num1*num2
+    elif operation == "/" :
+        message = num1/num2
+    else:
+        print("Invalid operation!")
+        return None
+    return message
